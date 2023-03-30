8541485274039768351826617bd111db56a8acdf
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-03-29 23:58:26 -0400

    Many changes made

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 let contentRight = document.getElementById("content-right");
 
 
-// Education Button Click
-let educationBtn = document.getElementById("education-btn");
+// // Education Button Click
+// let educationBtn = document.getElementById("education-btn");
 
-educationBtn.addEventListener("click", displayEducation);
+// educationBtn.addEventListener("click", displayEducation);
 
-function displayEducation() {
-    // Clear the html first
-    contentRight.innerHTML = "";
+// function displayEducation() {
+//     // Clear the html first
+//     contentRight.innerHTML = "";
 
-    // Pull the code from HoldingCode page
-    let content = document.getElementById("education-code").innerHTML;
+//     // Pull the code from HoldingCode page
+//     let content = document.getElementById("education-code").innerHTML;
 
-    // Replace the content
-    contentRight.innerHTML = content;
-}
+//     // Replace the content
+//     contentRight.innerHTML = content;
+// }
